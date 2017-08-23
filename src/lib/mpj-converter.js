746749f7aba81f16aede22cfe20126f1983a94d3
oigami <oigami013@yahoo.co.jp>
2017-08-23 23:49:15 +0900

    fix for IE (and safari)

--- src/lib/mpj-converter.js
+++ src/lib/mpj-converter.js
@@ -1,16 +1,16 @@
 ﻿import zlib from 'zlib';
+import { TextDecoder } from 'text-encoding';
 
 class Mpj {
     decompress(binary) {
         return new Promise((resolve, reject) => {
-            const buffer = new Uint8Array(binary);
             const mpjHeader = "MMM SaveFile";
-            const header = new TextDecoder("utf-16").decode(buffer.slice(0, 32));
+            const header = new TextDecoder("utf-16").decode(new Uint8Array(binary, 0, 32));
             if (mpjHeader !== header.slice(0, header.indexOf('\0'))) {
                 reject("Unknown file format. Header of input data: " + header.slice(0, header.indexOf('\0')));
                 return;
             }
-            const content = buffer.slice(36);
+            const content = new Uint8Array(binary, 36);
             zlib.inflate(content, (error, result) => {
                 if (error !== null) {
                     reject(error);
