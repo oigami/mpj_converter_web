f06f4cfbfbe9716e634b3fb646cc4faaaf276d36
oigami <oigami013@yahoo.co.jp>
2017-08-23 20:50:20 +0900

    fix bug

--- src/components/FileSelect.js
+++ src/components/FileSelect.js
@@ -14,21 +14,6 @@ export default class FileUpload extends React.Component {
         this.props.onChange({ files: acceptedFiles });
     }
 
-    errorHandler(evt) {
-        switch (evt.target.error.code) {
-            case evt.target.error.NOT_FOUND_ERR:
-                alert('File Not Found!');
-                break;
-            case evt.target.error.NOT_READABLE_ERR:
-                alert('File is not readable');
-                break;
-            case evt.target.error.ABORT_ERR:
-                break; // noop
-            default:
-                alert('An error occurred reading this file.');
-        }
-    }
-
     mouseOver() {
         this.setState({ mouseOver: true });
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -164,6 +164,21 @@ class Home extends React.Component {
         });
     }
 
+    errorHandler(evt) {
+        switch (evt.target.error.code) {
+            case evt.target.error.NOT_FOUND_ERR:
+                alert('File Not Found!');
+                break;
+            case evt.target.error.NOT_READABLE_ERR:
+                alert('File is not readable');
+                break;
+            case evt.target.error.ABORT_ERR:
+                break; // noop
+            default:
+                alert('An error occurred reading this file.');
+        }
+    }
+
     onChange = (e) => {
         e.files.forEach(file => {
             var reader = new FileReader();
